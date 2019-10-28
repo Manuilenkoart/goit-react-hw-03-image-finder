378d5983bcfb8bbc1394058678bd32c168bf8935
Manuilenkoart <manuilenkoart@gmail.com>
2019-10-28 17:22:43 +0200

    Updates

---
+++ precache-manifest.a222c487c4ce149488c0503080d4175c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047b209d573b0a9b56caa294dc4ba9de",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "639b36c1bcf7d0062fe4",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.122d5413.chunk.css"
+  },
+  {
+    "revision": "600878f6f37c658b8804",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b64d35f4.chunk.css"
+  },
+  {
+    "revision": "639b36c1bcf7d0062fe4",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b9f1a8eb.chunk.js"
+  },
+  {
+    "revision": "600878f6f37c658b8804",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f402e01b.chunk.js"
+  },
+  {
+    "revision": "26f980cacd962de52565",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9d9945f4.js"
+  }
+]);
